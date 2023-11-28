8df5a270bddea7d2481eabdf4347bb9a519f1aad
root-hezeiahs_PC <ok.hezekiahs@gmail.com>
2023-11-28 20:06:04 +0100

    last task

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        r1 = a + b
+        return r
+    else:
+        r2 = a * b - c
+        return r2
