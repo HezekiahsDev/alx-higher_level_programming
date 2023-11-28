e38821a1bac50d911e9a1138618321df8945eacc
root-hezeiahs_PC <ok.hezekiahs@gmail.com>
2023-11-28 16:04:58 +0100

    added new files

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#islower - checks is a character is lowercase
+#Return: true or false
+
+def islower(c):
+
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
