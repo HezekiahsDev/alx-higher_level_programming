abee6b0613ec57f50b8a473b652516f4cd066c12
root-hezeiahs_PC <ok.hezekiahs@gmail.com>
2023-12-01 22:29:17 +0100

    task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# this program imports module 'add' from a add_0
+
+if __name__ == "__main__":
+
+    from add_0 import add
+
+    a = 1
+    b = 2
+    c = add(a, b)
+
+    print("{} + {} = {}".format(a, b, c))
