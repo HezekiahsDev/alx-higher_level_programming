51ed3a83f2357f93fb2d644adf503ff6fa7eb8d5
root-hezeiahs_PC <ok.hezekiahs@gmail.com>
2023-12-01 22:56:12 +0100

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,11 +3,8 @@
 # this program imports module 'add' from a add_0
 
 if __name__ == "__main__":
-
     from add_0 import add
-
     a = 1
     b = 2
     c = add(a, b)
-
-    print('{} + {} = {}'.format( a, b, c))
+    print('{:d} + {:d} = {:d}'.format(a, b, c))
