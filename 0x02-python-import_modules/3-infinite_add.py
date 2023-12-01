d2f211f764f19c2a21fe81f5d232ce4c73872c56
root-hezeiahs_PC <ok.hezekiahs@gmail.com>
2023-12-02 00:09:15 +0100

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+
+    result = 0
+    args = sys.argv
+    
+    for i in range(len(args) - 1):
+        result = result + int(args[i + 1])
+    print("{}".format(result))
