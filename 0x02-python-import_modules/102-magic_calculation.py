3ba7407da4895584b6f93402ebe9c3cf9dfb4c70
root-hezeiahs_PC <ok.hezekiahs@gmail.com>
2023-12-02 01:34:28 +0100

    added new files

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
 
-    from magic_calculation_102 import add, sub
+    from calculation_1 import add, sub
     if a < b:
         magic = add(a, b)
         for i in range(4, 6):
