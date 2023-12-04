d398f548bf79879d177b3606ce12c9691ded4bde
root-hezeiahs_PC <ok.hezekiahs@gmail.com>
2023-12-04 17:55:12 +0100

    task 4

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    edit = ""
+    edit = my_string[:]
+    size = len(my_string)
+    i = 0
 
-    for char in my_string:
-        if char != 'C' and char != 'c':
-            edit = edit + char
+    for char in range(size):
+        if my_string[char] == 'C' or my_string[char] == 'c':
+            edit = edit[:(char - i):] + my_string[(char + 1):]
+            i = i + 1
         return edit
