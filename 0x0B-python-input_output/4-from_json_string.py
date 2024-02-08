7ebd448e46858d2e28be85575f53251fe606510f
Hezekiahs <ok.hezekiahs@gmail.com>
2024-02-08 13:03:59 +0100

    file added

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python3
-"""This script defines a function that converts a JSON to object"""
-
-
+#!/bin/python3
 import json
 
-
 def from_json_string(my_str):
-    """Returns the Python object representation of a JSON string"""
-    cnv_json_string = json.loads
-    return cnv_json_string
+    """
+    Convert a JSON string into a Python object
+
+    Args:
+    my_str (str): A JSON string representing a Python object.
+
+    Returns:
+    object: The Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
