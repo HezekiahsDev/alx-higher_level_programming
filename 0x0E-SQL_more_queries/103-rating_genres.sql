bd52cd05f8d351ee613b7ebde432f301028c9db2
Hezekiahs <111096175+Tychool@users.noreply.github.com>
2024-03-14 01:16:38 +0100

    Update 103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,12 @@
 --List genres by ratings thier ratings
 
-SELECT `g`.`name`, SUM(`r`.`rate`) AS `rating`
-FROM `tv_genres` AS `g`
-INNER JOIN `tv_show_genres` AS `s` ON `s`.`genre_id` = `g`.`id`
-INNER JOIN `tv_show_ratings` AS `r` ON `r`.`show_id` = `s`.`show_id`
-GROUP BY `g`.`name`
-ORDER BY `rating` DESC;
+SELECT `name`, SUM(`rate`) AS `rating`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
+
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = s.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
+ 
