c7be1cc28eaa37e29b820805751346968a0c66fe
Hezekiahs <ok.hezekiahs@gmail.com>
2024-04-01 13:28:14 +0100

    Task 0 and readme

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This script contains a function that initializes a matrix"""
 
+
 def square_matrix_simple(matrix=[]):
     res = [list(map(lambda x: x ** 2, row)) for row in matrix]
     return res
