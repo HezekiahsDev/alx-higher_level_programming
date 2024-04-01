1cd51fb86727fc2b586087e42a613ac719bb8fbd
Hezekiahs <ok.hezekiahs@gmail.com>
2024-04-01 13:45:15 +0100

    Task 2

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This script contains a funtion that finds common
+    elements in an array
+"""
+
+
+def common_elements(set_1, set_2):
+    res = (set_1 & set_2)
+    return res
