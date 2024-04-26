fd00cf9a5d4134b1ade6ddf2bda14e84698c8c08
Hezekiahs <ok.hezekiahs@gmail.com>
2024-04-27 00:28:38 +0100

    advance

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This script take a URL, send a request to display the size of resbonse body
 # URL is stored in $1
-curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
+
+curl -s "$1" | wc -c
