2c85c5196bbbe5305fe21fef6ded67be11358629
Hezekiahs <ok.hezekiahs@gmail.com>
2024-05-11 08:24:30 +0100

    task7

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// set a loop to print an array of strings
+
+let i;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < 3; i++) {
+  console.log(myString[i]);
+}
