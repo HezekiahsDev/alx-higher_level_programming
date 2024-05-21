c760e3e05681736072831ec3cbf54ebafad9aeec
Hezekiahs <ok.hezekiahs@gmail.com>
2024-05-21 08:45:43 +0100

    innit

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// print multiple strings (n = number of occurence)
+
+const n = Math.floor(Number(process.argv[2]));
+if (isNaN(n)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
