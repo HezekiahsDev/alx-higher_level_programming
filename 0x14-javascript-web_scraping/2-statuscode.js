6c1e76704dd84cd4d0653439949d2f4d1e4b703d
Hezekiahs <ok.hezekiahs@gmail.com>
2024-05-22 16:57:48 +0100

    status

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// GET status
+
+const req = require('request');
+const url = process.argv[2];
+req.get(url, function error, response) {
+	if (error) {
+		console.log(error);
+	}
+	else {
+		console.log(error);
+	}
+});
