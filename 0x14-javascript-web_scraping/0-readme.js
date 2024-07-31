c8be47c175be863aac33e52daad093d8f3145789
Hezekiahs <ok.hezekiahs@gmail.com>
2024-07-31 23:10:31 +0100

    debug

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-// read a file
-// require file system
-
+// Read a file
+// Require file system
 const filesys = require('fs');
 
-
-filesys.readFile(process.argv[2], 'utf-8',
-function (err, data) {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log(data);
+filesys.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });
